2e3bc6cb44c6ac96ec1f2f2fe54d2f3028874ada
linh3516915 <linh3516915@gmail.com>
2024-03-24 00:01:05 +0700

    baitaptuan3

---
+++ ketnoisql.php
@@ -0,0 +1,16 @@
+<?php
+$server = "localhost"; 
+$username = "root";    
+$password = "";        
+$database = "ketnoi"; 
+
+$conn = new mysqli($server, $username, $password, $database);
+
+if ($conn){
+    mysqli_query($conn, "SET NAMES 'utf8' ");
+    echo 'Đã kết nối thành công';
+} else {
+    echo "Kết nối thất bại";
+}   
+
+?>
